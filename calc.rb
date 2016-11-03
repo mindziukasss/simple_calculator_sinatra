b4fa6cc716eeff00b6aa7f6a50371035a8715998
mindziukasss <mindziukasss@gmail.com>
2016-11-03 17:27:15 +0200

    modified views files

--- calc.rb
+++ calc.rb
@@ -1,25 +1,26 @@
 require 'sinatra/base'
 
-
 class Calcapp < Sinatra::Base
 	set :static, true
 
-
-
-	get '/'do
+	get '/' do
 		'Hello :)'
+		erb :index
 		
+	end
+
+	get '/index' do  
+			erb :index
 	end
 
 	get '/add' do
 		erb :add
 	end
-
 	post '/add' do
 		@result = params[:first_no].to_f + params[:second_no].to_f
 		@first_no = params[:first_no].to_f
 		@second_no = params[:second_no].to_f
-		
+		erb :add
 	end
 
 	get '/sub' do
@@ -30,7 +31,7 @@ class Calcapp < Sinatra::Base
 		@result = params[:first_no].to_f - params[:second_no].to_f
 		@first_no = params[:first_no].to_f
 		@second_no = params[:second_no].to_f
-		
+		erb :sub
 	end
 
 	get '/multi' do
@@ -41,6 +42,7 @@ class Calcapp < Sinatra::Base
 		@result = params[:first_no].to_f * params[:second_no].to_f
 		@first_no = params[:first_no].to_f
 		@second_no = params[:second_no].to_f
+		erb :multi
 		
 	end
 
@@ -52,6 +54,7 @@ class Calcapp < Sinatra::Base
 		@result = params[:first_no].to_f / params[:second_no].to_f
 		@first_no = params[:first_no].to_f
 		@second_no = params[:second_no].to_f
+		erb :div
 		
 	end
 
